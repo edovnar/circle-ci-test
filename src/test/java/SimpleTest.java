41d7ddb3ab2328cab3f67c7c19c786d3bfd89125
edovnar <edovnar@solvd.com>
2021-03-04 17:58:50 +0300

    init commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,43 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class SimpleTest {
+
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final Random random = new Random();
+
+    @Test
+    public void hello() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void helloo() throws InterruptedException {
+        log.info("Sleeping for 10 seconds...");
+        TimeUnit.SECONDS.sleep(10);
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void helloOrNot() throws InterruptedException {
+        log.info("Sleeping for 5 seconds...");
+        TimeUnit.SECONDS.sleep(5);
+        int a = random.nextInt(3);
+        boolean greatRandom = a != 2;
+        Assert.assertTrue(greatRandom);
+    }
+
+    @Test
+    public void notOrHello() {
+        int a = random.nextInt(3);
+        boolean greatRandom = a == 2;
+        Assert.assertTrue(greatRandom);
+    }
+
+}
